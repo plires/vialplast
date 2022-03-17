4312c225c9b0c90a198753d1635565bd77ebc2ea
Pablo Lires <pablolires@gmail.com>
2022-03-17 16:53:23 -0300

    Actualizacion de plugins porto

--- wp-content/plugins/porto-functionality/elementor/widgets/blog.php
+++ wp-content/plugins/porto-functionality/elementor/widgets/blog.php
@@ -154,7 +154,7 @@ class Porto_Elementor_Blog_Widget extends \Elementor\Widget_Base {
 				'type'      => Controls_Manager::SELECT,
 				'label'     => __( 'Post Meta Type', 'porto-functionality' ),
 				'condition' => array(
-					'post_style' => array( 'list', 'hover_info', 'hover_info2' ),
+					'post_style' => array( 'hover_info', 'hover_info2' ),
 				),
 				'default'   => '',
 				'options'   => array(
--- wp-content/plugins/porto-functionality/porto-functionality.php
+++ wp-content/plugins/porto-functionality/porto-functionality.php
@@ -3,7 +3,7 @@
 Plugin Name: Porto Theme - Functionality
 Plugin URI: http://themeforest.net/user/p-themes
 Description: Adds functionality such as Shortcodes, Post Types and Widgets to Porto Theme
-Version: 2.2.2
+Version: 2.2.3
 Author: P-Themes
 Author URI: http://themeforest.net/user/p-themes
 License: GPL2

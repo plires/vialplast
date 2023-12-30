c1d3a2d3f95643abee690335345bee4c4b5b73aa
Pablo Lires <pablolires@gmail.com>
2023-12-29 21:12:28 -0300

    restyling del home

--- wp-content/themes/vialplast/inc/faja-distribuidores-marca.inc.php
+++ wp-content/themes/vialplast/inc/faja-distribuidores-marca.inc.php
@@ -1,75 +1,64 @@
 <!-- Distribuidores -->
 <section class="distribuidores container-fluid bg_secondary">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 offset-md-2 text-center">
-				<h3>¿Querés ser distribuidor de nuestros productos?</h3>
-				<a 
-					href="<?= esc_url( get_stylesheet_directory_uri() ) . '/distribuidores/'; ?>" 
-					class="btn btn-primary btn-out-black transition">Consultar <i class="fas fa-chevron-right"></i>
-				</a>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 text-center">
+        <h3>¿Querés ser distribuidor <br>de nuestros productos?</h3>
+        <a href="<?= esc_url( get_stylesheet_directory_uri() ) . '/distribuidores/'; ?>"
+          class="btn btn-primary btn-out-black transition">Consultar <i class="fas fa-chevron-right"></i>
+        </a>
+      </div>
+    </div>
+  </div>
 </section>
 <!-- Distribuidores end -->
 
 <!-- Nuestras Marcas -->
 <section class="nuestras_marcas container-fluid bg_secondary text-center">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<h3>Nuestras Marcas</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h3>Nuestras Marcas</h3>
 
-				<div class="content">
-					<div class="content_marcas">
-						<a href="https://depisos.com/" rel="noopener" target="_blank" >
-							<img 
-								class="marca img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/depisos.png'; ?>" 
-								alt="logo depisos">
-						</a>
-					</div>
+        <div class="content">
+          <div class="content_marcas">
+            <a href="https://depisos.com/" rel="noopener" target="_blank">
+              <img class="marca img-fluid"
+                src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/depisos.png'; ?>" alt="logo depisos">
+            </a>
+          </div>
 
-					<div class="content_marcas">
-						<a href="http://cantofil.com.ar/" rel="noopener" target="_blank" >
-							<img 
-								class="marca img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/cantofil.png'; ?>" 
-								alt="logo cantofil">
-						</a>
-					</div>
+          <div class="content_marcas">
+            <a href="http://cantofil.com.ar/" rel="noopener" target="_blank">
+              <img class="marca img-fluid"
+                src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/cantofil.png'; ?>" alt="logo cantofil">
+            </a>
+          </div>
 
-					<div class="content_marcas">
-						<a href="http://superfil.com.ar/" rel="noopener" target="_blank" >
-							<img 
-								class="marca img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/superfil.png'; ?>" 
-								alt="logo superfil">
-						</a>
-					</div>
+          <div class="content_marcas">
+            <a href="http://superfil.com.ar/" rel="noopener" target="_blank">
+              <img class="marca img-fluid"
+                src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/superfil.png'; ?>" alt="logo superfil">
+            </a>
+          </div>
 
-					<div class="content_marcas">
-						<a href="http://summit-plastic.net/" rel="noopener" target="_blank" >
-							<img 
-								class="marca img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/summit.png'; ?>" 
-								alt="logo summit">
-						</a>
-					</div>
+          <div class="content_marcas">
+            <a href="http://summit-plastic.net/" rel="noopener" target="_blank">
+              <img class="marca img-fluid"
+                src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/summit.png'; ?>" alt="logo summit">
+            </a>
+          </div>
 
-					<div class="content_marcas">
-						<a href="http://nordeco.com.ar/" rel="noopener" target="_blank" >
-							<img 
-								class="marca img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/nordeco.png'; ?>" 
-								alt="logo nordeco">
-						</a>
-					</div>
-				</div>
+          <div class="content_marcas">
+            <a href="http://nordeco.com.ar/" rel="noopener" target="_blank">
+              <img class="marca img-fluid"
+                src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/nordeco.png'; ?>" alt="logo nordeco">
+            </a>
+          </div>
+        </div>
 
-			</div>
-		</div>
-	</div>
+      </div>
+    </div>
+  </div>
 </section>
 <!-- Nuestras Marcas end -->
--- wp-content/themes/vialplast/page-home.php
+++ wp-content/themes/vialplast/page-home.php
@@ -3,92 +3,227 @@
 <!-- Home -->
 <section class="vialplast_home">
 
-	<!-- Header -->
-	<section class="row content_header">
-		<div class="col-md-12 p-0">
-			<div class="container h-100">
-				<div class="row h-100">
+  <!-- Categorias -->
+  <section class="categorias">
 
-					<div class="col-md-6 offset-md-3 col-lg-8 offset-lg-2">
-						<div>
-							<img 
-								class="logo_mobile img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/logo-mobile.png'; ?>" 
-								alt="logo mobile">
-							<img 
-								class="circulo img-fluid" 
-								src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/circulo.png'; ?>" 
-								alt="circulo">
-							<h1>Alta tecnología <br>al servicio de la <br>seguridad vial. </h1>
-							<a class="transition btn_to" href="#slide_home">
-								<img 
-									class="flecha img-fluid" 
-									src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/flecha.png'; ?>" 
-									alt="flecha">
-							</a>
-						</div>
-					</div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-10 offset-md-1">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="main_container">
+                <div class="content">
 
-					<div class="col-md-6 offset-md-3 col-lg-8 offset-lg-2">
-						<h3>Comprá todos nuestros <br>productos ONLINE.</h3>
-						<a 
-							class="ver_productos transition" 
-							href="<?= esc_url( get_stylesheet_directory_uri() ) . '/tienda/'; ?>">Ver productos
-						</a>
-					</div>
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_CONOS ) ); ?>">
+                    <article class="width_40 conos">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/conos-viales.jpg'; ?>"
+                        alt="conos viales">
+                      <h2 class="transition">CONOS <br>VIALES</h2>
+                      <h3>CONOS VIALES</h3>
+                    </article>
+                  </a>
 
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- Header end -->
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_DELINEADORES ) ); ?>">
+                    <article class="width_60 delineadores">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/delineadores-rebatibles.jpg'; ?>"
+                        alt="DELINEADORES REBATIBLES">
+                      <h2 class="transition">DELINEADORES <br>REBATIBLES</h2>
+                      <h3>DELINEADORES REBATIBLES</h3>
+                    </article>
+                  </a>
 
-	<!-- Slide Home -->
-	<?php include('inc/slide-home.inc.php'); ?>
+                </div>
 
-	<!-- Ultimos Productos -->
-	<?php include('inc/last-products.php'); ?>
+                <div class="content">
 
-	<!-- Buscas Otro Producto -->
-	<section class="buscar_otro_producto container-fluid">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h5>¿Buscás otro <br>producto?</h5>
-					<a 
-						href="<?= esc_url( get_stylesheet_directory_uri() ) . '/contacto/'; ?>" 
-						class="btn btn-primary transition">Escribínos <i class="fas fa-chevron-right"></i>
-					</a>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- Buscas Otro Producto end -->
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_TOPES_ESTACIONAMIENTO ) ); ?>">
+                    <article class="width_60 topes">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/topes-divisores.jpg'; ?>"
+                        alt="TOPES Y DIVISORES">
+                      <h2 class="transition">TOPES Y <br>DIVISORES</h2>
+                      <h3>TOPES Y DIVISORES</h3>
+                    </article>
+                  </a>
 
-	<!-- Constructoras y Municipios -->
-	<section class="constructoras_municipios container">
-		<div class="row">
-			<div class="col-md-8 offset-md-2 text-center">
-				<h3>Muchas empresas constructoras y municipios confian en VIALPLAST</h3>
-			</div>
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_VALLAS ) ); ?>">
+                    <article class="width_40 vallas">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/vallas-seguridad.jpg'; ?>"
+                        alt="VALLAS DE SEGURIDAD">
+                      <h2 class="transition">VALLAS DE <br>SEGURIDAD</h2>
+                      <h3>VALLAS DE SEGURIDAD</h3>
+                    </article>
+                  </a>
 
-			<div class="col-md-8 offset-md-2 content">
-				<h4>Constructoras y municipos</h4>
-				<a 
-					href="<?= esc_url( get_stylesheet_directory_uri() ) . '/constructoras-municipios/'; ?>" 
-					class="btn btn-primary btn-out-black transition">Contacto directo <i class="fas fa-chevron-right"></i>
-				</a>
-			</div>
-		</div>
-	</section>
-	<!-- Constructoras y Municipios end -->
+                </div>
 
-	<!-- Faja Caracterisitcas -->
-	<?php include('inc/faja-caracterisitcas.inc.php'); ?>
+                <div class="content">
 
-	<!-- Faja Distribuidores -->
-	<?php include('inc/faja-distribuidores-marca.inc.php'); ?>
-	
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_DEMARCACION ) ); ?>">
+                    <article class="width_30 demarcacion">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/demarcacion-grafismo.jpg'; ?>"
+                        alt="DEMARCACIÓN Y GRAFISMO">
+                      <h2 class="transition small">DEMARCACIÓN <br>Y GRAFISMO</h2>
+                      <h3>DEMARCACIÓN Y GRAFISMO</h3>
+                    </article>
+                  </a>
+
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_METROBUS ) ); ?>">
+                    <article class="width_40 metrobus">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/metrobus.jpg'; ?>"
+                        alt="METROBUS">
+                      <h2 class="transition small">METROBUS</h2>
+                      <h3>METROBUS</h3>
+                    </article>
+                  </a>
+
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_REDUCTORES_VELOCIDAD ) ); ?>">
+                    <article class="width_30 tachas_demarcatorias">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/tachas-demarcatorias.jpg'; ?>"
+                        alt="TACHAS DEMARCA TORIAS">
+                      <h2 class="transition small">TACHAS <br>DEMARCA <br>TORIAS</h2>
+                      <h3>TACHAS DEMARCATORIAS</h3>
+                    </article>
+                  </a>
+
+                </div>
+
+                <div class="content">
+
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_REDUCTORES_VELOCIDAD ) ); ?>">
+                    <article class="width_40 tachas">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/tachas-reductoras.jpg'; ?>"
+                        alt="TACHAS REDUCTORAS">
+                      <h2 class="transition small">TACHAS <br>REDUCTORAS</h2>
+                      <h3>TACHAS REDUCTORAS</h3>
+                    </article>
+                  </a>
+
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_REDUCTORES_VELOCIDAD ) ); ?>">
+                    <article class="width_60 reductores">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/reductores-velocidad.jpg'; ?>"
+                        alt="REDUCTORES DE VELOCIDAD">
+                      <h2 class="transition small">REDUCTORES <br>DE VELOCIDAD
+                        <h3>REDUCTORES DE VELOCIDAD</h3>
+                    </article>
+                  </a>
+
+                </div>
+
+                <div class="content">
+
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_ESQUINEROS ) ); ?>">
+                    <article class="width_60 esquineros">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/esquineros-guardas.jpg'; ?>"
+                        alt="ESQUINEROS Y GUARDAS DE SEGURIDAD">
+                      <h2 class="transition small">ESQUINEROS Y GUARDAS <br>DE SEGURIDAD</h2>
+                      <h3>ESQUINEROS Y GUARDAS DE SEGURIDAD</h3>
+                    </article>
+                  </a>
+
+                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_REDUCTORES_VELOCIDAD ) ); ?>">
+                    <article class="width_40 bolardos">
+                      <img class="transition"
+                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/bolardos-urbanos.jpg'; ?>"
+                        alt="BOLARDOS URBANOS">
+                      <h2 class="transition small">BOLARDOS <br>URBANOS</h2>
+                      <h3>BOLARDOS URBANOS</h3>
+                    </article>
+                  </a>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <!-- Categorias end -->
+
+  <!-- Header -->
+  <section class="row content_header">
+    <div class="col-md-12 p-0">
+      <div class="container h-100">
+        <div class="row h-100">
+          <h1>Alta tecnología <br>al servicio de la <br>seguridad vial. </h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Header end -->
+
+  <!-- Ultimos Productos -->
+  <?php include('inc/last-products.php'); ?>
+
+  <!-- Faja Jordan plas -->
+  <section class="jordan">
+    <div class="content">
+      <img class="img-fluid logo_jordan"
+        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/logo-jordan-plas.png'; ?>"
+        alt="logo jordan plas">
+      <img class="img-fluid bolardo_img"
+        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/bolardo.png'; ?>" alt="bolardo jordan plas">
+      <div class="data">
+        <h2>¡NUEVOS PRODUCTOS!</h2>
+        <p>
+          Sumamos a nuestro catálogo los productos de la reconocida marca Jordan-Plas, ampliando nuestro portofolio y
+          agregando calidad en pos de la seguridad vial.</p>
+      </div>
+    </div>
+  </section>
+  <!-- Faja Jordan plas end -->
+
+  <!-- Buscas Otro Producto -->
+  <section class="buscar_otro_producto container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h5>¿Buscás otro <br>producto?</h5>
+          <a href="<?= esc_url( get_stylesheet_directory_uri() ) . '/contacto/'; ?>"
+            class="btn btn-primary transition">Escribínos <i class="fas fa-chevron-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Buscas Otro Producto end -->
+
+  <!-- Constructoras y Municipios -->
+  <section class="constructoras_municipios container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 text-center">
+        <h3>Ya son varias empresas constructoras y
+          municipios los que confian en VIALPLAST</h3>
+      </div>
+
+      <div class="col-md-8 offset-md-2 content">
+        <h4>Constructoras <br> y municipos</h4>
+        <a href="<?= esc_url( get_stylesheet_directory_uri() ) . '/constructoras-municipios/'; ?>"
+          class="btn btn-primary btn-out-black transition">Contacto directo <i class="fas fa-chevron-right"></i>
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- Constructoras y Municipios end -->
+
+  <!-- Faja Caracterisitcas -->
+  <?php include('inc/faja-caracterisitcas.inc.php'); ?>
+
+  <!-- Faja Distribuidores -->
+  <?php include('inc/faja-distribuidores-marca.inc.php'); ?>
+
 </section>
 <!-- Home end -->
 

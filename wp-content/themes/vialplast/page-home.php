f72518ab56b7103347b7136bd0c938b1f3e23c98
Pablo Lires <pablolires@gmail.com>
2025-01-17 14:38:38 -0300

    agregado de baner en el home. correccion de centrado de elementos

--- wp-content/themes/vialplast/page-home.php
+++ wp-content/themes/vialplast/page-home.php
@@ -14,20 +14,34 @@
               <div class="main_container">
                 <div class="content">
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_CONOS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_KIT_INSTALACION)); ?>">
+                    <article data-aos="fade-up" class="width_100 kit">
+                      <img class="transition img-fluid"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/kit-instalacion.jpg'; ?>"
+                        alt="kit de instalacion">
+                      <h2 class="transition">KIT DE <br>INSTALACIÓN</h2>
+                      <h3>KIT DE INSTALACIÓN</h3>
+                    </article>
+                  </a>
+
+                </div>
+
+                <div class="content">
+
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_CONOS)); ?>">
                     <article data-aos="fade-up" class="width_40 conos">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/conos-viales.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/conos-viales.jpg'; ?>"
                         alt="conos viales">
                       <h2 class="transition">CONOS <br>VIALES</h2>
                       <h3>CONOS VIALES</h3>
                     </article>
                   </a>
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_DELINEADORES ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_DELINEADORES)); ?>">
                     <article data-aos="fade-up" class="width_60 delineadores">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/delineadores-rebatibles.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/delineadores-rebatibles.jpg'; ?>"
                         alt="DELINEADORES REBATIBLES">
                       <h2 class="transition">DELINEADORES <br>REBATIBLES</h2>
                       <h3>DELINEADORES REBATIBLES</h3>
@@ -38,20 +52,20 @@
 
                 <div class="content">
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_TOPES_ESTACIONAMIENTO ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_TOPES_ESTACIONAMIENTO)); ?>">
                     <article data-aos="fade-up" class="width_60 topes">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/topes-divisores.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/topes-divisores.jpg'; ?>"
                         alt="TOPES Y DIVISORES">
                       <h2 class="transition">TOPES Y <br>DIVISORES</h2>
                       <h3>TOPES Y DIVISORES</h3>
                     </article>
                   </a>
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_VALLAS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_VALLAS)); ?>">
                     <article data-aos="fade-up" class="width_40 vallas">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/vallas-seguridad.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/vallas-seguridad.jpg'; ?>"
                         alt="VALLAS DE SEGURIDAD">
                       <h2 class="transition">VALLAS DE <br>SEGURIDAD</h2>
                       <h3>VALLAS DE SEGURIDAD</h3>
@@ -62,30 +76,30 @@
 
                 <div class="content">
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_DEMARCACION ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_DEMARCACION)); ?>">
                     <article data-aos="fade-up" class="width_30 demarcacion">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/demarcacion-grafismo.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/demarcacion-grafismo.jpg'; ?>"
                         alt="DEMARCACIÓN Y GRAFISMO">
                       <h2 class="transition small">DEMARCACIÓN <br>Y GRAFISMO</h2>
                       <h3>DEMARCACIÓN Y GRAFISMO</h3>
                     </article>
                   </a>
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_METROBUS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_METROBUS)); ?>">
                     <article data-aos="fade-up" class="width_40 metrobus">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/metrobus.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/metrobus.jpg'; ?>"
                         alt="METROBUS">
                       <h2 class="transition small">METROBUS</h2>
                       <h3>METROBUS</h3>
                     </article>
                   </a>
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_TACHAS_DEMARCATORIAS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_TACHAS_DEMARCATORIAS)); ?>">
                     <article data-aos="fade-up" class="width_30 tachas_demarcatorias">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/tachas-demarcatorias.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/tachas-demarcatorias.jpg'; ?>"
                         alt="TACHAS DEMARCATORIAS">
                       <h2 class="transition small">TACHAS <br>DEMARCA <br>TORIAS</h2>
                       <h3>TACHAS DEMARCATORIAS</h3>
@@ -96,20 +110,20 @@
 
                 <div class="content">
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_TACHAS_REDUCTORAS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_TACHAS_REDUCTORAS)); ?>">
                     <article data-aos="fade-up" class="width_40 tachas">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/tachas-reductoras.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/tachas-reductoras.jpg'; ?>"
                         alt="TACHAS REDUCTORAS">
                       <h2 class="transition small">TACHAS <br>REDUCTORAS</h2>
                       <h3>TACHAS REDUCTORAS</h3>
                     </article>
                   </a>
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_REDUCTORES_VELOCIDAD ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_REDUCTORES_VELOCIDAD)); ?>">
                     <article data-aos="fade-up" class="width_60 reductores">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/reductores-velocidad.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/reductores-velocidad.jpg'; ?>"
                         alt="REDUCTORES DE VELOCIDAD">
                       <h2 class="transition small">REDUCTORES <br>DE VELOCIDAD
                         <h3>REDUCTORES DE VELOCIDAD</h3>
@@ -120,20 +134,20 @@
 
                 <div class="content">
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_ESQUINEROS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_ESQUINEROS)); ?>">
                     <article data-aos="fade-up" class="width_60 esquineros">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/esquineros-guardas.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/esquineros-guardas.jpg'; ?>"
                         alt="ESQUINEROS Y GUARDAS DE SEGURIDAD">
                       <h2 class="transition small">ESQUINEROS Y GUARDAS <br>DE SEGURIDAD</h2>
                       <h3>ESQUINEROS Y GUARDAS DE SEGURIDAD</h3>
                     </article>
                   </a>
 
-                  <a href="<?php echo esc_url( get_category_link( ID_CATEGORY_BOLARDOS ) ); ?>">
+                  <a href="<?php echo esc_url(get_category_link(ID_CATEGORY_BOLARDOS)); ?>">
                     <article data-aos="fade-up" class="width_40 bolardos">
                       <img class="transition"
-                        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/bolardos-urbanos.jpg'; ?>"
+                        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/bolardos-urbanos.jpg'; ?>"
                         alt="BOLARDOS URBANOS">
                       <h2 class="transition small">BOLARDOS <br>URBANOS</h2>
                       <h3>BOLARDOS URBANOS</h3>
@@ -171,10 +185,10 @@
   <section data-aos="fade-up" class="jordan">
     <div class="content">
       <img class="img-fluid logo_jordan"
-        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/logo-jordan-plas.png'; ?>"
+        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/logo-jordan-plas.png'; ?>"
         alt="logo jordan plas">
       <img class="img-fluid bolardo_img"
-        src="<?= esc_url( get_stylesheet_directory_uri() ) . '/img/home/bolardo.png'; ?>" alt="bolardo jordan plas">
+        src="<?= esc_url(get_stylesheet_directory_uri()) . '/img/home/bolardo.png'; ?>" alt="bolardo jordan plas">
       <div class="data">
         <h2>¡NUEVOS PRODUCTOS!</h2>
         <p>
@@ -191,7 +205,7 @@
       <div class="row">
         <div class="col-md-12">
           <h5>¿Buscás otro <br>producto?</h5>
-          <a href="<?= esc_url( get_stylesheet_directory_uri() ) . '/contacto/'; ?>"
+          <a href="<?= esc_url(get_stylesheet_directory_uri()) . '/contacto/'; ?>"
             class="btn btn-primary transition">Escribínos <i class="fas fa-chevron-right"></i>
           </a>
         </div>
@@ -210,7 +224,7 @@
 
       <div class="col-md-8 offset-md-2 content">
         <h4>Constructoras <br> y municipos</h4>
-        <a href="<?= esc_url( get_stylesheet_directory_uri() ) . '/constructoras-municipios/'; ?>"
+        <a href="<?= esc_url(get_stylesheet_directory_uri()) . '/constructoras-municipios/'; ?>"
           class="btn btn-primary btn-out-black transition">Contacto directo <i class="fas fa-chevron-right"></i>
         </a>
       </div>
@@ -227,5 +241,5 @@
 </section>
 <!-- Home end -->
 
-<script src="<?= esc_url( get_stylesheet_directory_uri() ) . '/js/init-aos.js'; ?>"></script>
+<script src="<?= esc_url(get_stylesheet_directory_uri()) . '/js/init-aos.js'; ?>"></script>
 <?php get_footer(); ?>
